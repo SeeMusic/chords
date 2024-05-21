1a0449d887d62089a96abe30adfd6cc2b907d17b
65岁退休Coder <70535483+DoubleXm@users.noreply.github.com>
2024-05-21 16:43:49 +0800

    feat: 增加日文语种 (#25)

--- packages/ui-component/src/locales/index.ts
+++ packages/ui-component/src/locales/index.ts
@@ -1,11 +1,13 @@
 import zhCn from './lang/zh-cn';
 import zhTw from './lang/zh-tw';
 import enUs from './lang/en-us';
+import jaJp from './lang/ja-jp';
 
 export {
   zhCn,
   zhTw,
-  enUs
+  enUs,
+  jaJp
 };
 
 export type TranslatePair = {
---
+++ packages/ui-component/src/locales/lang/ja-jp.ts
@@ -0,0 +1,21 @@
+export default {
+  name: 'ja-JP',
+  sop: {
+    common: {
+      cancel: 'キャンセル',
+      confirm: '確認',
+      packUp: '折りたたむ',
+      flareOut: '展開',
+      back: '戻る'
+    },
+    basicTable: {
+      emptyRuleValidatorText: '空白は許可されていません'
+    },
+    filterForm: {
+      selector: '検索またはフィルタリング'
+    },
+    pagination: {
+      total: '合計 {total} 件'
+    }
+  }
+};
