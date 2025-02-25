bea13402a302f2e0c6a772e6c5cbfa9563f5c85a
65岁退休Coder <70535483+DoubleXm@users.noreply.github.com>
2025-02-25 15:50:40 +0800

    feat: 越南语配置 (#27)

--- packages/ui-component/src/locales/index.ts
+++ packages/ui-component/src/locales/index.ts
@@ -2,12 +2,14 @@ import zhCn from './lang/zh-cn';
 import zhTw from './lang/zh-tw';
 import enUs from './lang/en-us';
 import jaJp from './lang/ja-jp';
+import viVn from './lang/vi-vn';
 
 export {
   zhCn,
   zhTw,
   enUs,
-  jaJp
+  jaJp,
+  viVn
 };
 
 export type TranslatePair = {
---
+++ packages/ui-component/src/locales/lang/vi-vn.ts
@@ -0,0 +1,21 @@
+export default {
+  name: 'vi-VN',
+  sop: {
+    common: {
+      cancel: 'Hủy bỏ',
+      confirm: 'Xác nhận',
+      packUp: 'Thu gọn',
+      flareOut: 'Mở rộng',
+      back: 'Quay lại'
+    },
+    basicTable: {
+      emptyRuleValidatorText: 'Không được để trống'
+    },
+    filterForm: {
+      selector: 'Tìm kiếm hoặc lọc'
+    },
+    pagination: {
+      total: 'Tổng cộng {total} mục'
+    }
+  }
+};
